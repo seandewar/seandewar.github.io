70b64ea04dd22a92bd26ff316b3ef44cf928766d
Sean Dewar <seandewar@users.noreply.github.com>
2021-07-09 11:26:05 +0100

    bethandjosh: RSVP warning & slight page updates

---
+++ js/wedding/rsvp.js
@@ -0,0 +1,18 @@
+function rsvpOnClick(rsvpElem) {
+    // don't navigate using the href automatically; it's there as a fallback
+    // in-case javascript is disabled
+    event.preventDefault();
+
+    const text = "Before RSVPing, if you are attending, ensure that you have "
+               + "read our menu below and have decided on what you would like "
+               + "to eat at the event."
+               + "\n\n"
+               + "If you have dietry requirements, you will be able to tell us "
+               + "these during the RSVP process."
+               + "\n\n"
+               + "Click OK when you are ready to continue.";
+
+    if (confirm(text)) {
+        window.location.href = rsvpElem.href;
+    }
+}
